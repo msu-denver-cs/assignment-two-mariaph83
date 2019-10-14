d22ebdfebea4a1968a597db8722d28eee814d2b7
Maria <mph5094@gmail.com>
2019-10-13 19:43:00 -0600

    Validation and search

--- app/models/make.rb
+++ app/models/make.rb
@@ -1,3 +1,7 @@
 class Make < ApplicationRecord
   has_many :cars, dependent: :destroy
+
+  validates :name, presence: true, format: { with: /\A[a-zA-z]+\z/, message: "should be only letters" }
+  validates :country, presence: true, format: { with: /\A[a-zA-z]+\z/, message: "should be only letters" }
+
 end
